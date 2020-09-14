8541160c429ed108b1dd145fe17ce65f3ce8fea2
Burak Aslan <burak@dropbox.com>
2020-09-14 18:51:00 -0400

    Updates

---
+++ precache-manifest.1ffc4b86b05d1412bc5ebdeaf026881e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c4e313b8e05da57013b02babfa8ac4",
+    "url": "/refraction-demo/index.html"
+  },
+  {
+    "revision": "fdebba00c382372ee064",
+    "url": "/refraction-demo/static/css/main.e772a918.chunk.css"
+  },
+  {
+    "revision": "696232f7fe66ae436bf2",
+    "url": "/refraction-demo/static/js/2.ce716967.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/refraction-demo/static/js/2.ce716967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdebba00c382372ee064",
+    "url": "/refraction-demo/static/js/main.9dff2c1e.chunk.js"
+  },
+  {
+    "revision": "4a41758facb74a8bc4e4",
+    "url": "/refraction-demo/static/js/runtime-main.eac90991.js"
+  },
+  {
+    "revision": "29d90451515706c97281beee7f350a0c",
+    "url": "/refraction-demo/static/media/aeonik-regular.29d90451.woff2"
+  },
+  {
+    "revision": "28d3296bc08239ad56c7160ea8dea249",
+    "url": "/refraction-demo/static/media/fragment.28d3296b.glsl"
+  },
+  {
+    "revision": "2d5e6faf5cf7818a860216b1456dffbc",
+    "url": "/refraction-demo/static/media/fragment.2d5e6faf.glsl"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/refraction-demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1468f7a2e986b34f89aef8aa295b7b77",
+    "url": "/refraction-demo/static/media/vertex.1468f7a2.glsl"
+  },
+  {
+    "revision": "2ded267df13ac0e6adf7cb018bb7cf13",
+    "url": "/refraction-demo/static/media/vertex.2ded267d.glsl"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/refraction-demo/precache-manifest.1ffc4b86b05d1412bc5ebdeaf026881e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/refraction-demo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
